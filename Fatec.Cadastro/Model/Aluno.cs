3b4397c647c87e0848a4ed5437bb1097bfd10eb4
Vinicius Carvalho <vinicius.cs01@gmail.com>
2017-08-27 19:36:20 -0300

    Criação de classes

---
+++ Fatec.Cadastro/Model/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Cadastro.Model
+{
+    class Aluno : Pessoa
+    {
+        public int RA { get; set; }
+        public List<string> Aulas { get; set; }
+    }
+}
---
+++ Fatec.Cadastro/Model/Professor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Cadastro.Model
+{
+    class Professor : Pessoa
+    {
+        public List<string> Materias { get; set; }
+    }
+}
